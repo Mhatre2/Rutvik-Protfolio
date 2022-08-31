bff383f5329e913434d7dd4a673f58c1f6ca2c75
Rownok Mahbub <rownok2001@gmail.com>
2022-08-31 19:26:53 +0600

    rownok

---
+++ my-project/pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Hero from '../Components/Home/Hero'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+ <>
+  <Hero/>
+ </>
+  )
+}
