73eaf517a6b3fa27ac90fad910a5202bd8365668
Rownok Mahbub <rownok2001@gmail.com>
2022-08-31 23:42:46 +0600

    hero section added

--- Components/Home/Hero.js
+++ Components/Home/Hero.js
@@ -1,13 +1,41 @@
-import React from 'react'
-
+import React from "react";
 const Hero = () => {
   return (
-    <div>
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container mx-auto flex justify-between items-center mt-20" >
+      <div className="">
+        <div className="flex items-center gap-3">
+          <hr className="w-10 bg-gray-200 " />
+          <span className="text-[18px] font-medium">
+            UI/UX Designer & Developer
+          </span>
+        </div>
+        <p className="text-[55px] font-bold leading-tight">
+          I’m Rownok Mahbub <br />
+          Designer And Developer
+        </p>
+        <p className="mt-5 text-md">
+          I'm creative designer based in Bangladesh, and I'm very passionate and
+          dedicated to my <br /> work.IYour Satisfaction is my success
+        </p>
+        <div className="flex gap-4 mt-10 ">
+       
+        <button className="font-medium text-[18px] flex items-center py-2 px-4 rounded-xl capitalize border border-black hover:bg-blue-600  relative gap-2 transition duration-300 hover:scale-110">Get In Touch
+          <img src="/Assets/rightarrow.svg" alt="" />
+          <span className="animate-ping absolute right-0 top-0 w-2 h-2  rounded-full bg-sky-400 opacity-75"></span>
+         </button>
+         <img className="cursor-pointer" src="/Assets/facebook.svg" alt="" />
+         <img className="cursor-pointer" src="/Assets/linkedin.svg" alt="" />
+         <img className="cursor-pointer" src="/Assets/behance.svg" alt="" />
+        </div>
+    
+      </div>
+      <div className="relative">
+        <img className="w-14 animate-bounce transition duration-3000 absolute top-0 -left-5" src="/Assets/figma.png" alt="" />
+        <img className="w-32 animate-bounce absolute bottom-0 -left-10" src="/Assets/nextjs.png" alt="" />
+        <img className="w-96 " src="/Assets/rownok.png" alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */ 
 module.exports = {
+  daisyui: {
+    themes: ["night","cupcake"],
+  },
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "4rem",
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
