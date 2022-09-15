6f574f6c10386020f589ce254203ca6187df2ce5
Rownok Mahbub <rownok2001@gmail.com>
2022-09-15 22:45:08 +0600

    wow awesome

--- Components/UIUX/FigmaPlugins.js
+++ Components/UIUX/FigmaPlugins.js
@@ -5,38 +5,38 @@ import { useScroll } from "../UseScroll";
 const Categories = [
   {
     id: 1,
-    title: "Foodepi - Food And Restaurant app",
-    text: "Mobile App",
+    title: "Pexels",
+    text: "Pexels is a free stock photo and video sharing millions of high-quality photos and videos.",
     image: "/Assets/uiux/pexels.svg",
   },
   {
     id: 2,
-    title: "Ecomme - E-shop ecommerce website",
-    text: "Mobile App",
+    title: "Remove BG",
+    text: "Remove the background of images automatically with just a single click ",
     image: "/Assets/uiux/removebg.svg",
   },
   {
     id: 3,
-    title: "Ecomme - E-shop ecommerce website",
-    text: "Mobile App",
+    title: "Blush",
+    text: "Blush works with mixable elements, so you can customize every variablee of an illustration",
     image: "/Assets/uiux/blus.svg",
   },
   {
     id: 1,
-    title: "Foodepi - Food And Restaurant app",
-    text: "Mobile App",
+    title: "Open Icon",
+    text: "A collection of 50,000+ handpicked icons for your design.",
     image: "/Assets/uiux/openicon.png",
   },
   {
     id: 1,
-    title: "Foodepi - Food And Restaurant app",
-    text: "Mobile App",
+    title: "Content Reel",
+    text: "Design layouts more efficiently by pulling text strings, images, and icons from one palette",
     image: "/Assets/uiux/contentreel.svg",
   },
   {
     id: 1,
-    title: "Foodepi - Food And Restaurant app",
-    text: "Mobile App",
+    title: "Unsplash",
+    text: "Insert beautiful images from Unsplash straight into your designs.",
     image: "/Assets/uiux/unsplash.svg",
   },
 ];
@@ -57,8 +57,8 @@ const FigmaPlugins = () => {
               My favourite Figma plugins
             </p>
             <p className="text-gray-400 max-w-xl text-center mt-3">
-              I made some useful UI Kits for designers of all levels that helps
-              you speed up and spice up your designs.
+              I use some useful figma plugins for designers of all levels that helps
+              me speed up and spice up my designs.
             </p>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 py-10">
