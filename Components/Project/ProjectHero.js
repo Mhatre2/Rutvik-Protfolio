ecde51aafffc3025eba299dc19c14d71c655d91f
Rownok <rownok2001@gmail.com>
2022-10-10 19:47:14 +0600

    rownok

--- Components/Project/ProjectHero.js
+++ Components/Project/ProjectHero.js
@@ -5,29 +5,43 @@ import { useScroll } from "../UseScroll";
 const ProjectHero = () => {
   const [element, controls] = useScroll();
   return (
-    <>
-
-    <div className="container lg:h-screen animate__animated animate__zoomIn animate__delay-.5s" ref={element}>
-        <motion.div className="bg-gradient-to-r from-[#032a41] via-[#650a45] to-[#032a41] mt-5 rounded-t-[50px] lg:rounded-[50px] flex flex-col justify-center items-center lg:-space-y-40"   variants={upAnimation}
-          animate={controls}
-          transition={{ delay: 0.2, type: "tween" }}>
-            <div className="flex flex-col items-center justify-center gap-10" >
-            <p className="uppercase text-white text-4xl lg:text-7xl text-center pt-10 lg:pt-20 font-bebasneo max-w-2xl">Ui design is the power to get any user attention</p>
-        <button className="font-medium uppercase text-[16px] flex items-center px-5 py-3 md:py-4 md:px-4 rounded-xl bg-gradient-to-r from-pink-300 to-purple-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass text-center">
-               Get Free Design 😃
-                <span className="animate-ping absolute right-0 top-0 w-3 h-3  rounded-full bg-gradient-to-r from-purple -400 to-purple-700 "></span>
-              </button>
-            </div>
-    
-        <div className="flex justify-center max-w-5xl">
-        <img className="w-full" src="/Assets/uiux/hero.svg" alt="" />
+    <div className="relative lg:h-screen lg:py-5 sm:pt-0 py-20 bg-gradient-to-r from-[#030332] via-[#100114] to-[#030332] animate__animated animate__zoomIn animate__delay-.5s" ref={element}>
+    <motion.div className="container my-auto grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-20 items-center lg:pt-24 "  variants={downAnimation}
+               animate={controls}
+               transition={{ delay: 0.2, type: "tween" }}>
+    <div
+          className="content"
+        
+        >
+        <div className="flex items-center gap-3">
+          <hr className="w-10 bg-white-500  "/>
+          <span className="md:text-[18px] font-medium text-gray-400 ">
+            UI/UX Designer  Developer 
+          </span>
         </div>
+        <p className="text-[40px] lg:text-[50px] xl:text-[57px] font-bold leading-tight mt-5 sm:mt-0 text-gray-100">
+          I'm Rownok Mahbub <br />
+          Designer And Developer 
+        </p>
+        <p className="mt-5 md:text-md text-gray-400">
+          I'm creative designer based in Bangladesh, and I'm very passionate and
+          dedicated to my <br className="hidden md:block" /> work.Your Satisfaction is my success
+        </p>
+        <div className="flex gap-4 mt-10 items-center">
        
-        </motion.div>
-      
+        <button className="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-purple-300 to-purple-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">Hire Me 😊
+          <span className="animate-ping absolute right-0 top-0 w-3 h-3  rounded-full bg-gradient-to-r from-purple -400 to-purple-700 "></span>
+         </button>
+       
+        </div>
+    
+      </div>
+      <div className=""  >
+    
+        <img className="" src="/Assets/main.svg" alt="" />
+      </div>
+    </motion.div>
     </div>
-   
-    </>
   )
 }
 
