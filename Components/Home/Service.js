78a320fd319c500ca9b7ab4ec2f65f39be76c4bf
Rownok <rownok2001@gmail.com>
2022-10-29 20:38:57 +0600

    resume updatred

--- Components/Home/Service.js
+++ Components/Home/Service.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import {GiCrossedBones} from'react-icons/gi'
 import {
   headerAnimation,
   imageAnimation,
@@ -8,6 +11,16 @@ import {
 import { useScroll } from "../UseScroll";
 import { AiOutlineHeart, AiOutlineEye } from "react-icons/ai";
 const Service = () => {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
   const [element, controls] = useScroll();
   const Categories = [
     {
@@ -68,7 +81,7 @@ const Service = () => {
           </span>
         </motion.div>
       
-        <div className="grid grid-cols-1 lg:grid-cols-2">
+        <div className="grid grid-cols-1 lg:grid-cols-2" onClick={openModal}>
           {Categories.map((category, i) => (
             <motion.div
               className=" lg:px-8 h-[500px] "
@@ -111,6 +124,54 @@ const Service = () => {
             </motion.div>
           ))}
         </div>
+        <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="relative w-full max-w-7xl transform  rounded-2xl bg-gradient-to-r from-slate-800 via-gray-600 to-slate-800 p-6 my-20 shadow-xl transition-all">
+                <div className="flex gap-4 mb-5">
+                  <img className="w-14 h-14 rounded-full object-cover" src="/Assets/rownokmahbub.jpg" alt="" />
+                  <div className="flex flex-col items-start ">
+                    <p className='text-xl font-semibold text-white capitalize'>Ecommerce website for a company</p>
+                    <p className='text-xl text-gray-100 capitalize'>rownok mahbub</p>
+                  </div>
+                </div>
+                <div className="absolute -right-4 -top-4 bg-red-200 p-3 rounded-full" onClick={closeModal}>
+                  <GiCrossedBones className='text-red-600 text-xl cursor-pointer'/>
+                </div>
+                <img className='w-full rounded-2xl' src="/Assets/big.png" alt="" />
+                <p className=' text-white text-4xl capitalize my-10 font-bebasneo'>lets Check the interactive prototype mode</p>
+                <iframe className='w-full rounded-2xl' min-width="300" height="580" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F6MAWji1dzRBNJLeF4p53pE%2FFoodepi-Mobile-App-Templete%3Fpage-id%3D366%253A2089%26node-id%3D524%253A3911%26viewport%3D168%252C-136%252C0.08%26scaling%3Dscale-down%26starting-point-node-id%3D524%253A3911%26show-proto-sidebar%3D1" allowfullscreen></iframe>
+                <div className="flex">
+                    
+                </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
         <div className="mx-auto container flex justify-center">
         <button className="font-medium text-[16px] flex items-center px-5 py-3 md:py-4 md:px-8 rounded-xl capitalize bg-gradient-to-r from-purple-300 to-purple-500 hover:from-pink-500 hover:to-yellow-500  relative gap-2 transition duration-300 hover:scale-105 text-white shadow-glass ">Load More
           <span className="animate-ping absolute right-0 top-0 w-3 h-3  rounded-full bg-gradient-to-r from-purple -400 to-purple-700 "></span>
