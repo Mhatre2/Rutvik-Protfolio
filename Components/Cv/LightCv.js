9f9656e2ebba6c7d7569a0f20745ad1c5eafcbbc
Rownok <rownok2001@gmail.com>
2022-10-29 23:57:30 +0600

    updatecv

--- Components/Cv/LightCv.js
+++ Components/Cv/LightCv.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const DarkCv = () => {
+const LightCv = () => {
   return (
     <div className='relative group'>
-        <img className='w-full rounded-xl md:group-hover:blur-md transition-all duration-500' src="/Assets/resumedark.svg" alt="" />
+        <img className='w-full rounded-xl md:group-hover:blur-md transition-all duration-500' src="/Assets/resumelight.svg" alt="" />
         <a
                   href="#"
                   className=" absolute top-1/2 w-full hidden md:group-hover:flex"
@@ -11,10 +11,10 @@ const DarkCv = () => {
                     <p className='font-medium text-[18px] text-gray-200 py-2 px-4 w-36 mx-auto capitalize bg-gradient-to-r from-purple-300 to-purple-500 hover:from-pink-500 hover:to-yellow-500 rounded-[20px] transition duration-300 hover:scale-105 flex justify-center'> Download CV</p>
                    
                 </a>
-        <p className='text-white flex justify-center mt-3 text-2xl capitalize'>Dark mode</p>
+        <p className='text-white flex justify-center mt-3 text-2xl capitalize'>light mode</p>
         <p className='font-medium text-[18px] text-gray-200 py-2 px-4 w-36 mx-auto capitalize bg-gradient-to-r from-purple-300 to-purple-500 hover:from-pink-500 hover:to-yellow-500 rounded-[20px] transition duration-300 hover:scale-105 flex justify-center mt-5 md:hidden'> Download CV</p>
     </div>
   )
 }
 
-export default DarkCv
+export default LightCv
--- pages/cv/index.js
+++ pages/cv/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import DarkCv from '../../Components/Cv/DarkCv'
+import LightCv from '../../Components/Cv/LightCv'
 
 const index = () => {
   return (
     <div className='container mx-auto items-center py-24 grid grid-cols-1 md:grid-cols-2 gap-20'>
-        <DarkCv/>
+        <LightCv/>
         <DarkCv/>
     </div>
   )
